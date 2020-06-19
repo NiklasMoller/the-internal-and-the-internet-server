fb33553ba6fec94d8d1ed1f64653b9c4c218b69a
NiklasMoller <niklasxmoller@gmail.com>
2020-06-19 13:50:32 +0200

    Add button to intercat with lottie animations

--- src/client/scripts/outsider.js
+++ src/client/scripts/outsider.js
@@ -6,4 +6,29 @@ var animation = bodymovin.loadAnimation({
   path: './resources/animations/outsider.json'
 })
 
-console.log('Playing outsider animation');
+
+
+/*
+function playVackert(){
+    anim.goToAndStop(vackertInFrame, true);
+    setTimeout(function(){ anim.goToAndPlay(vackertInFrame, true); }, 4000);
+}
+
+animation.currentFrame
+*/
+
+animation.addEventListener('enterFrame', () => {
+  console.log('enterFrame', anim.currentFrame);
+  if(animation.currentFrame == 926)
+  {
+  console.log('animation stop')
+  }
+});
+
+function buttonPressed(){
+  var message = document.getElementById("myInput").value;
+
+  console.log('Current frame is ' + animation.currentFrame);
+      animation.goToAndPlay(1600, true);
+
+}
